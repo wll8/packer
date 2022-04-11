e2868da0bd9682aba5e4d4a12008ae2fd6059121
wll8 <xwdaysme@gmail.com>
2022-04-11 11:27:53 +0800

    feat: 初步实现混入命令到某个包

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,8 @@ gulp.task(`ncc`, (cb) => { // 变更发布后的文件
   // ncc 需要依赖(偷懒使用符号链接)才能进行合并
   shell.exec(`npx shx ln -s ${__dirname}/src/node_modules ${__dirname}/dist/src/package/node_modules`)
   shell.exec(`npx ncc build ./dist/src/package/index.js -o ./dist/src/package/`)
+  // 移除 node_modules
+  shell.exec(`npx shx rm -rf ${__dirname}/dist/src/package/node_modules`)
 
   // 删除依赖声明 - 因为打包为单文件后就不需要此声明了
   package.dependencies = undefined
@@ -95,6 +97,35 @@ gulp.task(`test`, (cb) => { // 测试运行
   shell.exec(`cd dist && npm init -y`)
   shell.exec(`cd dist && npm i ${pkgPath}`)
   shell.exec(`cd dist && npx ${cliName}`)
+  cb()
+})
+
+gulp.task(`minxin`, async (cb) => { // 混入到某个包
+  const name = `shelljs`
+  const package = require(`./node_modules/${name}/package.json`)
+
+  console.log(`package`, package)
+  shell.exec(`cd ./node_modules/${name} && npm pack`)
+  shell.exec(`npx shx mv node_modules/${name}/${name}-${package.version}.tgz ./dict/`)
+
+  const compressing = require('compressing');
+  await compressing.tgz.uncompress(`./dist/${name}-${package.version}.tgz`, `./dist/${name}/`).catch(console.log)
+
+  shell.exec(`npx shx cp -rf ./dist/src/package ./dict/${name}/_minxin`)
+
+  package.bin = {
+    ...package.bin,
+    ...Object.entries(bin).reduce((acc, [key, val]) => {
+      acc[key] = `./_minxin/index.js`
+      return acc
+    }, {}),
+  }
+  package.files = [
+    ...package.files,
+    `_minxin`,
+  ]
+  fs.writeFileSync(`./dict/${name}/package.json`, JSON.stringify(package, null, 2))
+  shell.exec(`cd ./dict/${name}/ && npm pack`)
   cb()
 })
 
